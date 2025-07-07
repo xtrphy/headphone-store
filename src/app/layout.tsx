7c85b76b7de08406baaff8880e851f44b23cf5f4
xtrphy <perepeckinvlad@gmail.com>
2025-07-07 20:00:45 +0500

    Create navbar below Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
         <html lang="en">
             <body className={`${montserrat.variable} antialiased`}>
                 <Header />
-                {children}
+                <main>
+                    {children}
+                </main>
                 <Footer />
             </body>
         </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <h1 className="text-7xl">Hello World!</h1>
-        </div>
+        <section className="flex items-center justify-center">
+            <div className="flex items-center justify-center bg-main-green w-full gap-[75px] h-[81px] text-white text-[20px] font-semibold">
+                <Link href="/features" className="link">Features</Link>
+                <Link href="/products" className="link">Products</Link>
+                <Link href="/blog" className="link">Blog</Link>
+                <Link href="/about" className="link">About</Link>
+            </div>
+        </section>
     );
 }
